6da1120b60a149c3328578c3ed9d916e66a33db3
awpgaming <surajwithawp@gmail.com>
2023-11-08 02:30:30 +0530

    optimize

--- imageOpt.js
+++ imageOpt.js
@@ -1,4 +1,7 @@
-export default function cloudinaryLoader({ src, width, quality }) {
-    const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
-    return `/dist${src}`
-  }
+// export default function cloudinaryLoader({ src, width, quality }) {
+//     const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
+//     return `/dist${src}`
+//   }
+export default function myImageLoader({ src, width, quality }) {
+  return `${src}?w=${width}&q=${quality || 75}`
+}
--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
-  // distDir: 'dist',
-  // trailingSlash: true,
-  // assetPrefix: '.',
-  // images: {
-  //   loader: 'custom',
-  //   loaderFile: './imageOpt.js',
-  // },
-};
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
+  assetPrefix: '.',
+  images: {
+    loader: 'custom',
+    loaderFile: './imageOpt.js',
+  },
+ 
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
